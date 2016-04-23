43523673bd467c369ab38fbb3ef4a554784b631a
Richard Griffiths <griff182uk@yahoo.co.uk>
2016-04-23 15:40:27 +0100

    start

---
+++ OneASPNetFromScratch/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace OneASPNetFromScratch.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ OneASPNetFromScratch/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OneASPNetFromScratch.Startup))]
+namespace OneASPNetFromScratch
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
